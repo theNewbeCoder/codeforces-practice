46761cad2c85e8fd72e9c86512f1b66f9d3c250d
MD ONIK HASAN <onik.brur.cse15@gmail.com>
2025-05-21 12:03:15 +0600

    Add files via upload

---
+++ A_Beautiful_Matrix.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int m[5][5];
+
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            scanf("%d",&m[i][j]);
+        }
+    }
+
+    int fi,fj;
+for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            if(m[i][j]==1)
+            {
+                fi=i; fj=j;
+            }
+        }
+    }
+
+    printf("%d", abs(fi-2)+abs(fj-2));
+
+
+    return 0;
+}
