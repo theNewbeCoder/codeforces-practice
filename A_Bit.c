9adf487363b692753f004bcdd36e6c321cd0934d
MD ONIK HASAN <onik.brur.cse15@gmail.com>
2025-05-21 11:57:22 +0600

    Add files via upload

---
+++ A_Bit.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int t,i,x=0;
+    scanf("%d",&t);
+    char s[4];
+
+
+    for(i=0;i<t;i++)
+    {
+        scanf("%s",s);
+        if(s[1]=='+' || s[0]=='+')
+        {
+            x++;
+        }
+        else
+        {
+            x--;
+        }
+
+    }
+    printf("%d",x);
+
+    return 0;
+}
